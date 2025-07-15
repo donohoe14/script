c60826e86e39ba83d0215833d6d2b27714408ba8
donohoe14 <donohoe14@aol.com>
2025-07-15 19:27:30 +0100

    Update helloworld.sh

--- helloworld.sh
+++ helloworld.sh
@@ -1,18 +1,2 @@
-pipeline{
-    agent any
-    stages{
-        stage('checkout stage'){
-            steps{
-                checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/donohoe14/script.git']])
-            }
-        }
-        stage('script usage'){
-            steps{
-                sh'''
-                chmod +x helloworld.sh
-                sh helloworld.sh
-                '''
-            }
-        }
-    }
-}
+#!/bin/bash
+echo "Hellow World!"
